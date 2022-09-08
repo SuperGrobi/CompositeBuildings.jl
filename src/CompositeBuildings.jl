d5c5a39d39f6428e89b1e8c476108b43b0ef65e8
Henrik Wolf <henrik-wolf@freenet.de>
2022-09-08 14:54:04 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.29

---
+++ src/CompositeBuildings.jl
@@ -0,0 +1,5 @@
+module CompositeBuildings
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using CompositeBuildings
+using Test
+
+@testset "CompositeBuildings.jl" begin
+    # Write your tests here.
+end
