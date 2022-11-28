b849e8322561c064521dbd9bb56b34c86ad455a3
Henrik Wolf <henrik-wolf@freenet.de>
2022-11-28 16:34:57 +0100

    removed other osm building code

--- src/CompositeBuildings.jl
+++ src/CompositeBuildings.jl
@@ -1,38 +1,12 @@
 module CompositeBuildings
 
 using CoolWalksUtils
-using LightOSM
-using LightXML
-using JSON
-using HTTP
-using Formatting
 using ArchGDAL
 using GeoInterface
-using GeoInterfaceRecipes
 using DataFrames
 using GeoDataFrames
 using GeoFormatTypes
 using ProgressMeter
-
-export SimpleBuilding,
-    BuildingPart,
-    CompositeBuilding
-include("types.jl")
-
-export download_composite_osm_buildings
-include("building_downloaders.jl")
-
-
-export composite_buildings_from_object,
-    composite_buildings_from_file,
-    composite_buildings_from_download,
-    to_dataframe,
-    osm_dfs_from_object,
-    osm_dfs_from_file,
-    osm_dfs_from_download,
-    buildings_from_test_area,
-    dfs_from_test_area
-include("building_parsers.jl")
 
 export relate_buildings
 include("relate_buildings.jl")
@@ -42,5 +16,4 @@ include("load_other_sources.jl")
 
 export cast_shadow
 include("ShadowCasting.jl")
-
 end
