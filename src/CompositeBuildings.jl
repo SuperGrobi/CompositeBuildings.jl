9abbad2c1c53a320bef4eddc034f02efd288d193
Henrik Wolf <henrik-wolf@freenet.de>
2023-02-13 17:29:05 +0100

    added function to load the new york dataset

--- src/CompositeBuildings.jl
+++ src/CompositeBuildings.jl
@@ -11,7 +11,7 @@ using ProgressMeter
 export relate_buildings
 include("relate_buildings.jl")
 
-export load_british_shapefiles
+export load_british_shapefiles, load_new_york_shapefiles
 include("OtherLoaders.jl")
 
 export cast_shadow
