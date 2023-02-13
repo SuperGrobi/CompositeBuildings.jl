5da300b0150389e371f8660cdd83c239bedd4fa7
Henrik Wolf <henrik-wolf@freenet.de>
2023-02-13 18:08:19 +0100

    dropped missing height polygons

--- test/current.jl
+++ test/current.jl
@@ -13,8 +13,27 @@ c = load_british_shapefiles(joinpath(datapath, "clifton/clifton.shp"); bbox=(min
 
 b = load_new_york_shapefiles(joinpath(datapath, "manhattan/manhattan.shp"))
 
+p = ArchGDAL.pointonsurface.(b.geometry)
+g = b.geometry
+names(b)
+
+heights = Set(b.heightroof)
+gdf = groupby(b, :heightroof)
+
+msh = filter(i -> nrow(i) > 6, gdf)
+
+msh[1]
+
 smalls = filter(:heightroof => h -> h isa Missing, b)
 
 
+@showprogress 1 "well..." map(p) do p1
+    mapreduce(+, g) do g1
+        ArchGDAL.contains(g1, p1)
+    end
+end
+
+
+
 plot(c.geometry[1])
-draw(b.geometry; stroke=true)
+draw(msh[1].geometry; stroke=true, figure_params=Dict(:location => (40.78, -73.97), :zoom_start => 12))
