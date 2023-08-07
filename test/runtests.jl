be1592978ebf31b18daf63f97f73a170fb428fa9
Henrik Wolf <henrik-wolf@freenet.de>
2023-08-07 22:15:20 +0200

    added info logging

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,9 @@ using TestItemRunner
 
 
 if haskey(ENV, "RUNNING_IN_GITHUB_CI") && ENV["RUNNING_IN_GITHUB_CI"]
+    @info "Github CI environment detected"
     @run_package_tests filter = ti -> !(:skipci in ti.tags) verbose = true
 else
+    @info "Running tests locally"
     @run_package_tests verbose = true
 end
