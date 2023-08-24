03ece2ac6d58bf5026d6832d32b8cb52ebb663b6
Henrik Wolf <henrik-wolf@freenet.de>
2023-08-24 17:48:05 +0200

    fixed ci environment detection

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using TestItemRunner
 
-if haskey(ENV, "RUNNING_IN_GITHUB_CI") && ENV["RUNNING_IN_GITHUB_CI"]
+if haskey(ENV, "RUNNING_IN_GITHUB_CI") && ENV["RUNNING_IN_GITHUB_CI"] == "true"
     @info "Github CI environment detected"
     @run_package_tests filter = ti -> !(:skipci in ti.tags) verbose = true
 else
